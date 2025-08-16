582f676975f8f0dbca5e76ca3684a3415295da44
Omar Khan <omar.bartokhan@gmail.com>
2025-08-16 15:23:23 -0500

    Add placeholder comments for future work

--- voice_recordings/urls.py
+++ voice_recordings/urls.py
@@ -4,7 +4,11 @@ from . import views
 
 urlpatterns = [
     path("", views.form, name="form"),
-    # TODO: obfuscate recording IDs
+    # FIXME: Including integer primary keys in the URL is not ideal.
+    # - Gives a sense of many recordings we have
+    # - Makes it too easy to guess the URL of someone else's recording
+    #
+    # Use an obfuscated identifier instead, like a UUID.
     path("recording/<int:recording_id>/", views.recording, name="recording"),
     path(
         "recording/<int:recording_id>/call_started/",
